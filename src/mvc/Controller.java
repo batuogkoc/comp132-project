b2022b68b93ce4a760160e76ad5e00427a427df3
batuogkoc <bgunduz21@ku.edu.tr>
2022-05-19 01:37:03 +0300

    pledge of honour

--- src/mvc/Controller.java
+++ src/mvc/Controller.java
@@ -5,6 +5,15 @@ import backend.Group;
 import backend.User;
 import frontend.Login;
 
+/* Pledge of Honor *************************************************************************************
+I hereby certify that I have completed this programming project on my own without any help from
+anyone else. The effort in the project thus belongs completely to me. I did not search for a
+solution, or I did not consult to any program written by others or did not copy any program from
+other sources. I read and followed the guidelines provided in the project description.
+READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID
+SIGNATURE: <Batu Orhun Gunduz, 79886>
+************************************************************************************************************/
+
 /**
  * controller class for the mvc structure. Manipulates View and Model to give functionality to the program.
  * Mostly involved in switching between windows
